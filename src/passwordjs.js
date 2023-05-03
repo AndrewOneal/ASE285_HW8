0db933566c6b225da4f8f404f056f8c01bd2de5a
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2023-05-03 18:50:41 -0400

    updated passwordjs.js

--- src/passwordjs.js
+++ src/passwordjs.js
@@ -1,19 +1,31 @@
 'use strict'
-const fs = require('fs');
-const util = require('./utility')
+const {readFile, hash} = require('./utility');
 
 function passwordjs() {
     if (process.argv.length != 5) return 'false';
 
-    var filename = process.argv[2]
+    var encodedData = readFile(process.argv[2]);
     var email = process.argv[3]
     var password = process.argv[4]
+pup
+    let match;
 
-    ???
+    encodedData.forEach((line, index) => {
+        if (line.includes(email)) {
+            match = encodedData[index];
+        }
+    });
+
+    if (match !== undefined || null) {
+        return hash(password) === match.split(':')[1];
+    }
+
+    return false;
+
 }
 
 if (require.main === module) {
     console.log(passwordjs()) // print out true or false
 }
 
-module.exports = {???};
+module.exports = { passwordjs };
